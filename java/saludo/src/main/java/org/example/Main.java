f8732708121f13096bc1b1d0fd0de17fcf28a13e
enciso <encisolf901@gmail.com>
2022-06-27 20:37:33 -0500

    cambio java basic

---
+++ java/saludo/src/main/java/org/example/Main.java
@@ -0,0 +1,21 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        String saludo = "hello world";
+        byte num1 = 1;
+        int num2 = 100;
+        long num3 = 10000000;
+
+        char letter = 'a';
+
+        boolean flag = true;
+
+        double num4 = 3.3;
+        float num5 = 3.4f;
+
+
+
+    }
+}
